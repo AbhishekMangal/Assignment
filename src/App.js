63cab938c1f975df1c5a0b76b48c71641b3df483
AbhishekMangal <149200926+AbhishekMangal@users.noreply.github.com>
2024-07-17 20:04:24 +0530

    ini commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [matrix, setMatrix] = useState(Array(9).fill('zinc-300'));
+  const [clicks, setClicks] = useState([]);
+
+  const handleClick = (index) => {
+    if (matrix[index] !== 'zinc-300') return;
+
+    const newMatrix = [...matrix];
+    newMatrix[index] = 'green';
+
+    setMatrix(newMatrix);
+    const clickOrder = [...clicks, index];
+
+    setClicks(clickOrder);
+
+    if (clickOrder.length === 9) {
+      setTimeout(() => {
+        clickOrder.forEach((idx, i) => {
+          setTimeout(() => {
+            newMatrix[idx] = 'orange';
+            setMatrix([...newMatrix]);
+          }, i * 500);
+        });
+      }, 1000);
+    }
+  };
+
+  const handleRestart = () => {
+    setMatrix(Array(9).fill('zinc-300'));
+    setClicks([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='text-center text-white text-5xl pb-5'>Assignment</h1>
+      <div className='container grid grid-cols-3 gap-3 pt-5 w-80 place-content-center h-screen ms-auto me-auto'>
+        {matrix.map((color, index) => (
+          <div
+            key={index}
+            className={`rounded-sm w-24 h-24 p-0 m-0 ${color === 'zinc-300' ? 'bg-zinc-300' : color === 'green' ? 'bg-green' : 'bg-orange'}`}
+            onClick={() => handleClick(index)}
+          >
+          </div>
+        ))}
+      </div>
+      <button onClick={handleRestart} className='text-black bg-zinc-200 mt-4'>Restart</button>
+    </>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        green: '#10b981',
+        orange: '#f97316',
+        white: '#f8fafc',
+        'zinc-300': '#d1d5db',
+      },
+    },
+  },
+  plugins: [],
+}
